00753360250b79c15f8f02c130f0a30a34889e43
coredeveloper <coredeveloper@gmail.com>
2023-04-09 17:53:13 +0200

    delete old

--- neumorphism_old/itwillrock_ui_neumorphism/lib/constants/animations.dart
+++
@@ -1,3 +0,0 @@
-const Duration loginAnimationDuration = Duration(milliseconds: 2000);
-const Duration onboardingAnimationDuration = Duration(milliseconds: 500);
-const Duration genericDuration = Duration(milliseconds: 750);
